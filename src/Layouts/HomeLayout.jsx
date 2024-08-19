79556a8b846db332887882f8deb5ac36bfa4cd74
SAGRSURI <sagarsuriofficial@gmail.com>
2024-08-19 12:26:37 +0530

    Added login Signup buttons to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,21 @@
 import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import Footer from '../Components/Footer'
 
 function HomeLalyout({ children }){
+
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    // for checking use is logged in 
+     const isLoggedIn = useSelector((state)=> state?.auth?.isLoggedIn)
+
+     //for displaying the options acc to role
+     const role =useSelector((state)=> state?.auth?.role);
 
     function changeWidth(){
         const drawerSide = document.getElementsByClassName('drawer-side')
@@ -17,6 +28,14 @@ function HomeLalyout({ children }){
 
         const drawerSide = document.getElementsByClassName('drawer-side')
         drawerSide[0].style.width = '0';
+    }
+
+    function handleLogout(e){
+        e.preventDefault();
+
+        // const res = await dispatch(logout())
+        // if(res?.payload?.success)
+            navigate('/')
     }
 
     return(
@@ -43,6 +62,11 @@ function HomeLalyout({ children }){
                         <li>
                             <Link to='/'>Home</Link>
                         </li>
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to='/admin/dashboard'>Admin DashBoard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to='/courses'>All Courses</Link>
                         </li>
@@ -52,6 +76,30 @@ function HomeLalyout({ children }){
                         <li>
                             <Link to='/about'>About Us</Link>
                         </li>
+                        {!isLoggedIn &&(
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary bg-blue-800 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to='/login'>Login</Link>
+                                    </button>
+                                    <button className="btn-secondary bg-pink-800 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to='/login'>SignUp</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+                        {isLoggedIn &&(
+                            <li className="absolute bottom-4 w-[90%]">
+                                <div className="w-full flex items-center justify-center">
+                                    <button className="btn-primary bg-blue-800 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to='/login'>Profile</Link>
+                                    </button>
+                                    <button className="btn-secondary bg-pink-800 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link onClick={handleLogout}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
 
