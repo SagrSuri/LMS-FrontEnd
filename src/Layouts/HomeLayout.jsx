2e49f6fafd7c506fce47b7a09270cd5e8de5b72c
SAGRSURI <sagarsuriofficial@gmail.com>
2024-08-24 21:17:45 +0530

    Added complete signup flow

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,42 +3,40 @@ import { FiMenu } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
-import Footer from '../Components/Footer'
+import Footer from "../Components/Footer";
 
-function HomeLalyout({ children }){
+function HomeLalyout({ children }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
+    // for checking use is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+    //for displaying the options acc to role
+    const role = useSelector((state) => state?.auth?.role);
 
-    // for checking use is logged in 
-     const isLoggedIn = useSelector((state)=> state?.auth?.isLoggedIn)
-
-     //for displaying the options acc to role
-     const role =useSelector((state)=> state?.auth?.role);
-
-    function changeWidth(){
-        const drawerSide = document.getElementsByClassName('drawer-side')
-        drawerSide[0].style.width = 'auto';
+    function changeWidth() {
+        const drawerSide = document.getElementsByClassName("drawer-side");
+        drawerSide[0].style.width = "auto";
     }
 
     function hideDrawer() {
-        const element = document.getElementsByClassName('drawer-toggle')
+        const element = document.getElementsByClassName("drawer-toggle");
         element[0].checked = false;
 
-        const drawerSide = document.getElementsByClassName('drawer-side')
-        drawerSide[0].style.width = '0';
+        const drawerSide = document.getElementsByClassName("drawer-side");
+        drawerSide[0].style.width = "0";
     }
 
-    function handleLogout(e){
+    function handleLogout(e) {
         e.preventDefault();
 
         // const res = await dispatch(logout())
         // if(res?.payload?.success)
-            navigate('/')
+        navigate("/");
     }
 
-    return(
+    return (
         <div className="min-h-[90vh]">
             <div className="drawer absolute left-0 z-50 w-fit ">
                 <input className="drawer-toggle" id="my-drawer" type="checkbox" />
@@ -46,13 +44,13 @@ function HomeLalyout({ children }){
                     <label htmlFor="my-drawer" className="cursor-pointer relative">
                         <FiMenu
                             onClick={changeWidth}
-                            size={'32px'}
-                            className="font-bold text-white m-4 " />
+                            size={"32px"}
+                            className="font-bold text-white m-4 "
+                        />
                     </label>
                 </div>
                 <div className="drawer-side w-0">
-                    <label htmlFor="my-drawer" className="drawer-overlay">
-                    </label>
+                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-100 text-base-content relative">
                         <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
@@ -60,30 +58,30 @@ function HomeLalyout({ children }){
                             </button>
                         </li>
                         <li>
-                            <Link to='/'>Home</Link>
+                            <Link to="/">Home</Link>
                         </li>
-                        {isLoggedIn && role === 'ADMIN' && (
+                        {isLoggedIn && role === "ADMIN" && (
                             <li>
-                                <Link to='/admin/dashboard'>Admin DashBoard</Link>
+                                <Link to="/admin/dashboard">Admin DashBoard</Link>
                             </li>
                         )}
                         <li>
-                            <Link to='/courses'>All Courses</Link>
+                            <Link to="/courses">All Courses</Link>
                         </li>
                         <li>
-                            <Link to='/contact'>Contact Us</Link>
+                            <Link to="/contact">Contact Us</Link>
                         </li>
                         <li>
-                            <Link to='/about'>About Us</Link>
+                            <Link to="/about">About Us</Link>
                         </li>
                         {!isLoggedIn && (
                             <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
                                     <button className="btn-primary hover:bg-blue-500 bg-blue-800 px-4 py-1 font-semibold rounded-md w-full">
-                                        <Link to='/login'>Login</Link>
+                                        <Link to="/login">Login</Link>
                                     </button>
                                     <button className="btn-secondary bg-pink-800 hover:bg-pink-600 px-4 py-1 font-semibold rounded-md w-full">
-                                        <Link to='/login'>SignUp</Link>
+                                        <Link to="/signup">SignUp</Link>
                                     </button>
                                 </div>
                             </li>
@@ -92,7 +90,7 @@ function HomeLalyout({ children }){
                             <li className="absolute bottom-4 w-[90%]">
                                 <div className="w-full flex items-center justify-center">
                                     <button className="btn-primary bg-blue-800 px-4 py-1 font-semibold rounded-md w-full">
-                                        <Link to='/login'>Profile</Link>
+                                        <Link to="/login">Profile</Link>
                                     </button>
                                     <button className="btn-secondary bg-pink-800 px-4 py-1 font-semibold rounded-md w-full">
                                         <Link onClick={handleLogout}>Logout</Link>
@@ -102,12 +100,11 @@ function HomeLalyout({ children }){
                         )}
                     </ul>
                 </div>
-
             </div>
             {children}
             <Footer />
         </div>
-    )
+    );
 }
 
 export default HomeLalyout;
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,16 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
 
+import axiosInstance from "../../Helpers/axiosInstance";
 const initialState = {
-    isLoggedIn: localStorage.getItem('isLoggedIn') || false,
-    role: localStorage.getItem('role') || '',
-    data: localStorage.getItem('data') || {},
-}
+  isLoggedIn: localStorage.getItem("isLoggedIn") || false,
+  role: localStorage.getItem("role") || "",
+  data: localStorage.getItem("data") || {},
+};
+
+export const creatAccount = createAsyncThunk("/auth/signup", async (data) => {
+  try {
+    const res = axiosInstance.post("user/register", data);
+    toast.promise(res, {
+      loading: "wait creating your account ",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed To create Account",
+    });
+
+    return (await res).data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+});
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-})
+  name: "auth",
+  initialState,
+  reducers: {},
+});
 
 // export const {} = authSlice.actions;
 
